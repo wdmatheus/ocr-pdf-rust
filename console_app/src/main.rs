becf0640f46e5934593116930be9befac3939a13
wdmatheus <wendel@builtcode.com.br>
2024-12-10 11:23:41 -0300

    updated compose.yaml

--- console_app/src/main.rs
+++ console_app/src/main.rs
@@ -3,7 +3,7 @@ async fn main() {
     let buffer = std::fs::read("/Users/wdmatheus/Desktop/oficio-requisitorio9.pdf")
         .expect("Failed to read file");
 
-    let ocr_processor = ocr_pdf::ocr_processor::OcrProcessor::default();
+    let ocr_processor = ocr_pdf::ocr_processor::OcrProcessor;
 
     match ocr_processor.extract_text_from_buffer(&buffer).await {
         Ok(ocr) => println!("{}", ocr),
